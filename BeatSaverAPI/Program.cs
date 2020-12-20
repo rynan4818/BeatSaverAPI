b3815711741348fde37e62096575bc0601e3d7c1
rynan4818 <14249877+rynan4818@users.noreply.github.com>
2020-12-21 01:29:57 +0900

    初期インポート

---
+++ BeatSaverAPI/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BeatSaverSharp;
+
+namespace BeatSaverAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpOptions options = new HttpOptions
+            {
+                ApplicationName = "Test Client",
+                Version = new Version(1, 0, 0),
+            };
+
+            // Use this to interact with the API
+            BeatSaver beatsaver = new BeatSaver(options);
+        }
+    }
+}
